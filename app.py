076be34da23448332b3a3459a2f962a0462e2af2
delaworla <worlanuel@hotmail.com>
2023-09-07 14:01:50 +0000

    changes

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
 from fastapi import FastAPI, HTTPException, Query
 from datetime import datetime
+from pytz import timezone
 import os
 
 app = FastAPI()
 
 @app.get("/api")
-def api(slack_name: str, track: str):
-    # Get the current day of the week.
-    current_day = datetime.datetime.now().weekday()
+async def get_info(
+    slack_name: str = Query(..., description="Slack name"),
+    track: str = Query(..., description="Track"),
+):
+    try:
+        # Get current day of the week in the 'UTC' timezone
+        utc_timezone = timezone("UTC")
+        current_day = datetime.now(utc_timezone).strftime("%A")
 
-    # Get the current UTC time.
-    current_utc_time = datetime.datetime.utcnow()
+        # Get current UTC time with validation of +/-2 minutes
+        utc_time = datetime.now(utc_timezone)
+        utc_offset_minutes = utc_time.utcoffset().total_seconds() / 60  # Convert to minutes
 
-    # Get the GitHub URL of the file being run.
-    file_url = "https://github.com/delaworla/backend_track/main.py"
+        if abs(utc_offset_minutes) > 2:
+            raise HTTPException(status_code=400, detail="Invalid UTC offset")
 
-    # Get the GitHub URL of the full source code.
-    repo_url = "https://github.com/delaworla/backend_track"
+        # Get GitHub URLs from environment variables
+        github_url_file = os.environ.get('GITHUB_URL_FILE', 'Not available')
+        github_url_source = os.environ.get('GITHUB_URL_SOURCE', 'Not available')
 
-    # Create the response payload.
-    
-    
+        # Create the JSON response
+        response_data = {
+            "Slack name": slack_name,
+            "Current day of the week": current_day,
+            "Current UTC time": utc_time.strftime('%Y-%m-%d %H:%M:%S %Z'),
+            "Track": track,
+            "GitHub URL of the file being run": github_url_file,
+            "GitHub URL of the full source code": github_url_source,
+        }
 
-    return response
+        return response_data
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
