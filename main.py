956ef3fb8b1e8b444c3afd3994e9500cb18dc6b8
delaworla <worlanuel@hotmail.com>
2023-09-08 06:57:42 +0000

    date format

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def api(slack_name: str, track: str):
     response_data = {
             "Slack name": slack_name,
             "Current day of the week": current_day,
-            "Current UTC time": utc_time.strftime("%Y-%m-%d %H:%M:%S %Z"),
+            "Current UTC time": utc_time.strftime("%Y-%m-%dT%H:%M:%S %Z"),
             "Track": track,
             "GitHub URL of the file being run": f"https://github.com/delaworla/backend_track/main.py",
             "GitHub URL of the full source code": f"https://github.com/delaworla/backend_track",
